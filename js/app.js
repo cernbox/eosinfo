7c695d1fdae68d3788506f5330e703a5c18103b2
root <root@cbox-lbwebdev-01.cern.ch>
2020-06-22 10:02:33 +0200

    hack: notify message

--- js/app.js
+++ js/app.js
@@ -6,3 +6,7 @@
 		OCA.EosInfo = {};
 	}
 })();
+
+$(document).ready(function () {
+	OC.Notification.showHtml("<a href='https://cern.service-now.com/service-portal?id=outage&n=OTG0057336' target='_blank'><b>OTG0057336 - CERNBox Web UI will be unvailable Saturday 27th from 7:30 to 14:30</a>", {timeout: 10, type: "error"});	
+});
